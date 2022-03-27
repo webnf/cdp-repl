047e6c78ebc118cbfa85f18e4347c13542d591ff
Herwig Hochleitner <herwig@bendlas.net>
2022-03-28 01:52:38 +0200

    cleanup

--- src/webnf/cdp_repl.clj
+++ src/webnf/cdp_repl.clj
@@ -1,6 +1,5 @@
 (ns webnf.cdp-repl
-  (:require [clj-chrome-devtools.core :as dt]
-            [clj-chrome-devtools.events :as de]
+  (:require [clj-chrome-devtools.events :as de]
             [clj-chrome-devtools.impl.connection :as dtc]
             (clj-chrome-devtools.commands
              [runtime :as dr]
@@ -13,11 +12,13 @@
             [clojure.core.match :as m :refer [match]]
             [clojure.java.io :as io]
             [clojure.data.json :as json]
+            [cljs.closure]
+            [cljs.compiler]
             [cljs.build.api :as bapi]
 
             [cljs.repl :as repl]
-            [cljs.stacktrace :as st]
-            [clj-http.client :as hc]))
+            [cljs.util]
+            [cljs.stacktrace :as st]))
 
 (defmacro dochan [[binding chan] & body]
   `(let [chan# ~chan]
@@ -31,62 +32,39 @@
   {:pre [(not (contains? m k))]}
   (assoc m k v))
 
-#_(def c (dtc/connect-url "ws://127.0.0.1:7777/inspect/e53a2171f13dfbe"))
-#_(dt/set-current-connection! c)
-#_(.close c)
-
-#_(def ctxs (atom {}))
-#_(dochan [{{:keys [context]} :params}
-         (de/listen c :runtime :execution-context-created)]
-        (swap! ctxs assoc-once (:name context) context))
-#_(dr/enable c {})
-
-#_(dr/get-properties {:object-id (:object-id (:result (dr/evaluate {:expression "global" :context-id 2})))})
-
-#_(dr/evaluate c {:expression "throw '';" :context-id 2})
-
-#_(dr/compile-script c {:expression "debugger"
-                      :source-url "<input>"
-                      :execution-context-id 2
-                      :persistScript true})
-#_(dr/run-script c {:script-id "115"
-                  :execution-context-id 2
-                  :return-by-value false
-                  :generate-preview true})
-
 (defn handle-break! [connection {:as break
                                  {[{:keys [call-frame-id]}] :call-frames} :params}]
-  (def BREAK break)
+  #_(def BREAK break)
   (try
     (match [(dd/evaluate-on-call-frame connection {:call-frame-id call-frame-id
-                                                   :expression "CLJS_DEVTOOLS_REQUEST.method"})]
+                                                   :expression "WEBNF_CDP_REPL_REQUEST.method"})]
            [{:result {:type "string" :value "read-script"}}]
            (match [(dd/evaluate-on-call-frame connection {:call-frame-id call-frame-id
-                                                          :expression "CLJS_DEVTOOLS_REQUEST.script"})]
+                                                          :expression "WEBNF_CDP_REPL_REQUEST.script"})]
                   [{:result {:type "string" :value script}}]
                   (do (prn :read-script script)
                       (dd/set-variable-value connection {:call-frame-id call-frame-id
                                                          :scope-number 0
-                                                         :variable-name "CLJS_DEVTOOLS_RESULT"
+                                                         :variable-name "WEBNF_CDP_REPL_RESULT"
                                                          :new-value {:type "string"
                                                                      :value (slurp (or (io/resource (str "bot/" script))
                                                                                        (io/resource script)))}})))
            [{:result {:type "string" :value "console"}}]
            (println (str (->
                           (dd/evaluate-on-call-frame connection {:call-frame-id call-frame-id
-                                                                 :expression "CLJS_DEVTOOLS_REQUEST.level"})
+                                                                 :expression "WEBNF_CDP_REPL_REQUEST.level"})
                           :result :value)
                          ":")
                     (->
                      (dd/evaluate-on-call-frame connection {:call-frame-id call-frame-id
-                                                            :expression "CLJS_DEVTOOLS_REQUEST.values.join(\", \")"})
+                                                            :expression "WEBNF_CDP_REPL_REQUEST.values.join(\", \")"})
                      :result :value)))
     (dd/resume connection {})
     (catch Exception e
-      (def ERROR-BREAK break)
+      #_(def ERROR-BREAK break)
       (crepl/pst e))
-    (finally
-      #_(dd/resume connection {}))))
+    #_(finally
+        (dd/resume connection {}))))
 
 #_(dochan [break (de/listen :debugger :paused)]
         (handle-break! c break))
@@ -113,13 +91,13 @@
   (let [module (get (:modules opts) (:module-name opts))
         asset-path (or (:asset-path opts)
                        (cljs.util/output-directory opts))
-        closure-defines (clojure.data.json/write-str (:closure-defines opts))]
+        closure-defines (json/write-str (:closure-defines opts))]
     (str "
 (function(global){
   function makeLogger(level) {
     return function() {
-      var CLJS_DEVTOOLS_RESULT;
-      var CLJS_DEVTOOLS_REQUEST = {
+      var WEBNF_CDP_REPL_RESULT;
+      var WEBNF_CDP_REPL_REQUEST = {
         method: \"console\",
         level: level,
         values: Array.from(arguments)
@@ -128,12 +106,11 @@
       return null;
     };
   }
-  global.devtoolsConsole = {
+  global.webnfCdpReplConsole = {
     log: makeLogger(\"log\"),
     warn: makeLogger(\"warn\"),
     error: makeLogger(\"error\")
   };
-  global.screepsConsole = global.console;
 })(this);
 
 var CLOSURE_BASE_PATH = \"goog/\";
@@ -141,15 +118,15 @@ var CLOSURE_UNCOMPILED_DEFINES = " closure-defines ";
 var CLOSURE_IMPORT_SCRIPT = (function(global) {
   var sentinel = new Object();
   return function(src) {
-    var CLJS_DEVTOOLS_RESULT = sentinel;
-    var CLJS_DEVTOOLS_REQUEST = {
+    var WEBNF_CDP_REPL_RESULT = sentinel;
+    var WEBNF_CDP_REPL_REQUEST = {
       method: \"read-script\",
       script: src
     };
     debugger;
-    if(sentinel === CLJS_DEVTOOLS_RESULT) { throw new Error(\"No loader attached\"); }
-    global.console = global.devtoolsConsole; // will be overwritten in tick
-    eval.call(global, CLJS_DEVTOOLS_RESULT);
+    if(sentinel === WEBNF_CDP_REPL_RESULT) { throw new Error(\"No loader attached\"); }
+    global.console = global.webnfCdpReplConsole; // will be overwritten in tick
+    eval.call(global, WEBNF_CDP_REPL_RESULT);
     return true;
   };
 })(this);
@@ -171,7 +148,7 @@ CLOSURE_IMPORT_SCRIPT(\"cljs_deps.js\");
 (defrecord DevtoolsEnv [url context handle-break! state]
   repl/IJavaScriptEnv
   (-setup [this opts]
-    (prn :setup)
+    #_(prn :setup)
     (let [connection (dtc/connect-url url)
           pl (de/listen connection :debugger :paused)
           cl (de/listen connection :console :message-added)
@@ -201,7 +178,7 @@ CLOSURE_IMPORT_SCRIPT(\"cljs_deps.js\");
   (-evaluate [_ _ _ js]
     (to-result
      (dr/evaluate (:connection @state)
-                  {:expression (str "global.console = global.devtoolsConsole; " js)
+                  {:expression (str "global.console = global.webnfCdpReplConsole; " js)
                    :context-id context
                    :generate-preview true})))
   (-load [_ provides url]
@@ -218,7 +195,7 @@ CLOSURE_IMPORT_SCRIPT(\"cljs_deps.js\");
                        :execution-context-id context
                        :generate-preview true}))))
   (-tear-down [this]
-    (prn :tear-down)
+    #_(prn :tear-down)
     (let [{:keys [connection debug-pauses console-messages context-created context-destroyed]} @state]
       (dd/disable connection {})
       (dc/disable connection {})
@@ -293,51 +270,3 @@ CLOSURE_IMPORT_SCRIPT(\"cljs_deps.js\");
                :infer-externs true}))
   )
 
-(comment
- (hc/get "https://screeps.com/api/auth/me"
-         {:headers {"X-Token" "a4qb9edcd-f68d-428c-8ae0-0c00f23931e1"}})
-
- (def login-token
-   (->
-    (hc/post "http://localhost:21025/api/auth/signin"
-             {:headers {:accept "application/json"
-                        :content-type "application/json"}
-              :body (json/json-str {:email "bendlas"
-                                    :password "steyr"})})
-    :body
-    (json/read-str :key-fn keyword)
-    :token))
-
- (->
-  (hc/get "http://localhost:21025/api/auth/me"
-          {:headers {"X-Token" login-token
-                     "X-Username" login-token}}
-          #_{:basic-auth ["bendlas" "steyr"]}
-          #_{:headers {"authorization" (hc/basic-auth-value ["bendlas" "steyr"])}})
-  :body
-  (json/read-str :key-fn keyword))
-
- (hc/get "http://localhost:21025/api/user/code"
-         {:as :json
-          :headers {"X-Token" login-token
-                    "X-Username" login-token}})
-
- (hc/post "http://localhost:21025/api/user/code"
-          {:as :json
-           :headers {"X-Token" login-token
-                     "X-Username" login-token}
-           :accept :json
-           :content-type :json
-           :body (json/json-str
-                  {:branch "default"
-                   :modules
-                   {:main "
-module.exports.loop = function () {
-  if (undefined === global.CLJS_LOOP) {
-    console.log(\"Waiting for CLJS_LOOP to be defined\");
-  } else {
-    CLJS_LOOP();
-  }
-}
-"}})})
- )
